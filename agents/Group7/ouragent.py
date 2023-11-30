e2783d5dd76a24a6ec70b055ff8e09ccdb05f546
Jin Wang <jin.wang-12@student.manchester.ac.uk>
2023-11-30 15:28:57 +0000

    introduce basic structures

---
+++ agents/Group7/ouragent.py
@@ -0,0 +1,97 @@
+import socket
+from random import choice
+
+
+class Ouragent():
+    HOST = "127.0.0.1"
+    PORT = 1234
+
+    def __init__(self, board_size=11):
+        self.s = socket.socket(
+            socket.AF_INET, socket.SOCK_STREAM
+        )
+        self.s.connect((self.HOST, self.PORT))
+        self.board_size = board_size
+        self.board = []
+        self.colour = ""
+        self.turn_count = 0
+
+    def run(self):
+        while True:
+            data = self.s.recv(1024)
+            if not data:
+                break
+            # print(f"{self.colour} {data.decode('utf-8')}", end="")
+            if self.interpret_data(data):
+                break
+        # print(f"Naive agent {self.colour} terminated")
+
+    def interpret_data(self, data):
+        messages = data.decode("utf-8").strip().split("\n")
+        messages = [x.split(";") for x in messages]
+        # print(messages)
+        for s in messages:
+            if s[0] == "START":
+                self.board_size = int(s[1])
+                self.colour = s[2]
+                self.board = [
+                    [0] * self.board_size for i in range(self.board_size)]
+                if self.colour == "R":
+                    self.make_move()
+            elif s[0] == "END":
+                return True
+            elif s[0] == "CHANGE":
+                if s[3] == "END":
+                    return True
+                elif s[1] == "SWAP":
+                    self.colour = self.opp_colour()
+                    if s[3] == self.colour:
+                        self.make_move()
+                elif s[3] == self.colour:
+                    action = [int(x) for x in s[1].split(",")]
+                    self.board[action[0]][action[1]] = self.opp_colour()
+                    self.make_move()
+        return False
+
+    def make_move(self):
+        # run a alpha beta prunning minimax search that use neural network as heuristic provider
+        # print(f"{self.colour} making move")
+        if self.colour == "B" and self.turn_count == 0:
+            if :
+                self.s.sendall(bytes("SWAP\n", "utf-8"))
+            else:
+                # same as below
+                choices = []
+                for i in range(self.board_size):
+                    for j in range(self.board_size):
+                        if self.board[i][j] == 0:
+                            choices.append((i, j))
+                pos = choice(choices)
+                self.s.sendall(bytes(f"{pos[0]},{pos[1]}\n", "utf-8"))
+                self.board[pos[0]][pos[1]] = self.colour
+        else:
+            choices = []
+            for i in range(self.board_size):
+                for j in range(self.board_size):
+                    if self.board[i][j] == 0:
+                        choices.append((i, j))
+            pos = choice(choices)
+            self.s.sendall(bytes(f"{pos[0]},{pos[1]}\n", "utf-8"))
+            self.board[pos[0]][pos[1]] = self.colour
+        self.turn_count += 1
+
+    def opp_colour(self):
+        """Returns the char representation of the colour opposite to the
+        current one.
+        """
+        if self.colour == "R":
+            return "B"
+        elif self.colour == "B":
+            return "R"
+        else:
+            return "None"
+
+
+if (__name__ == "__main__"):
+    agent = Ouragent()
+    agent.run()

6b39b02e3efe8df0120cc5506c9fd15c4255df0f
Jin Wang <jin.wang-12@student.manchester.ac.uk>
2023-12-12 17:29:24 +0000

    small update

--- agents/Group7/runFile.py
+++ agents/Group7/runFile.py
@@ -2,18 +2,18 @@ import subprocess
 import time
 
 # 训练脚本的路径
-training_script_path = r"D:\COMP34111-group-project\agents\Group7\model_update_11.py"
+training_script_path = r"D:\COMP34111-group-project\agents\Group7\model_11 with_other_agent_update.py"
 
 # 迭代次数
-num_iterations = 17
+num_iterations = 6*2
 
 # 每次迭代之间的等待时间（秒）
-wait_time = 2
+wait_time = 0.1
 
 for i in range(num_iterations):
     # 运行训练脚本
     print("Starting training iteration", i + 1)
-    subprocess.run(['python', training_script_path])
+    subprocess.run(['python', training_script_path]) #about 10 mins
 
     # 等待一段时间
     print(f"Waiting for {wait_time} seconds before next iteration...")
